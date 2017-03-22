85d92d832ffee022744a2a435cc0838cc5bf0833
Guillaume Ballet <gballet@gmail.com>
2017-03-22 09:22:43 -0400

    Parent element is created before each unit test

--- test/widget-test.js
+++ test/widget-test.js
@@ -3,7 +3,14 @@ const Widget = require("js/widget")
 describe("Widget class test", () => {
     const fakeX = 90;
     const fakeY = 267;
-    const fakeParent = document.createElement("div");
+
+    beforeEach(() => {
+        this.fakeParent = document.createElement("div");
+    });
+
+    afterEach(() => {
+        delete this.fakeParent;
+    });
 
     it("should be able to instantiate a 'g' widget", () => {
         expect(() => {
@@ -12,19 +19,19 @@ describe("Widget class test", () => {
     });
 
     it("should set the parent correctly", () => {
-        let widget = new Widget(fakeParent, "g");
+        let widget = new Widget(this.fakeParent, "g");
         expect(widget.parent).to.be(fakeParent);
     });
 
     it("should retain the x and y coordinates", () => {
-        let widget = new Widget(fakeParent, "g");
+        let widget = new Widget(this.fakeParent, "g");
         widget.set_position(fakeX, fakeY)
         expect(widget.x).to.be(fakeX);
         expect(widget.y).to.be(fakeY);
     });
 
     it("should set the `transform` attribute to reflect (x,y) coordinates", () => {
-        let widget = new Widget(fakeParent, "g");
+        let widget = new Widget(this.fakeParent, "g");
         widget.set_position(fakeX, fakeY);
         widget.draw();
         const transform_attr = widget.widget.getAttribute("transform");
